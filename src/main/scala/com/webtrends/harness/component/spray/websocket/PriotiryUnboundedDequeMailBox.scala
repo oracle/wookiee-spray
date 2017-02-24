15f468023133016a70c12623edbfcec1bd0340e1
Jeff Berger <Jeff.Berger@Webtrends.com>
2017-02-24 12:56:08 -0800

    Allow web sockets to replace backed up messages with an updated version

--- src/main/scala/com/webtrends/harness/component/spray/websocket/PriotiryUnboundedDequeMailBox.scala
+++ src/main/scala/com/webtrends/harness/component/spray/websocket/PriotiryUnboundedDequeMailBox.scala
@@ -17,29 +17,43 @@ import com.typesafe.config.Config
  *
  * Based on UnboundedDequeBasedMailbox from Akka.
  */
-abstract class PriorityUnboundedDequeMailbox extends MailboxType
-with ProducesMessageQueue[UnboundedDequeBasedMailbox.MessageQueue] {
+abstract class PriorityUnboundedDequeMailbox extends MailboxType with ProducesMessageQueue[UnboundedDequeBasedMailbox.MessageQueue] {
   def this(settings: ActorSystem.Settings, config: Config) = this()
 
   final override def create(owner: Option[ActorRef], system: Option[ActorSystem]): MessageQueue =
-    new PriorityUnboundedDequeMailbox.MessageQueue(priority)
+    new PriorityUnboundedDequeMailbox.MessageQueue(priority, isDupe)
 
   /**
    * When true, the queue will place this envelope at the front of the
    * queue (as if it was just stashed).
    */
   def priority(e: Envelope): Boolean
+
+  /**
+    * When true, the new envelope will replace the last envelope in the queue
+    * When false, it will be added to the end
+    */
+  def isDupe(lastEnv: Envelope, newEnv: Envelope): Boolean
 }
 
 /**
  * Copied from https://github.com/smootoo/simple-spray-websockets/blob/master/src/main/scala/org/suecarter/websocket/PriorityUnboundedDequeMailbox.scala
  */
 object PriorityUnboundedDequeMailbox {
-  class MessageQueue(priority: Envelope => Boolean) extends LinkedBlockingDeque[Envelope] with UnboundedDequeBasedMessageQueue {
+
+  class MessageQueue(priority: Envelope => Boolean, isDupe: (Envelope, Envelope) => Boolean) extends LinkedBlockingDeque[Envelope] with UnboundedDequeBasedMessageQueue {
     final val queue = this
 
     override def enqueue(receiver: ActorRef, handle: Envelope): Unit =
-      if (priority(handle)) super.enqueueFirst(receiver, handle)
-      else super.enqueue(receiver, handle)
+      if (priority(handle)) {
+        super.enqueueFirst(receiver, handle)
+      } else if (size > 0 && isDupe(handle, queue.peekLast())) {
+          super.removeLast()
+          super.enqueue(receiver, handle)
+      }
+      else {
+        super.enqueue(receiver, handle)
+      }
   }
+
 }
